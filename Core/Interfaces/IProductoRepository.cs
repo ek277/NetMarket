7d5401dab392277e3326e4c450ebbbbdf35b248c
Ehm27 <erick@coolhumans.team>
2021-11-16 11:17:32 -0600

    Add project files.

---
+++ Core/Interfaces/IProductoRepository.cs
@@ -0,0 +1,17 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Interfaces
+{
+    public interface IProductoRepository
+    {
+        Task<Producto> GetProductoByIdAsync(int id); // Dentro de los <> se indica qué tipo de objeto queremos devolver
+        Task<IReadOnlyList<Producto>> GetProductosAsync(); 
+
+
+    }
+}
